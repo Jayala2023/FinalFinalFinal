40dde7959b8c8836cd69c976d8ba72ca03a605f6
Jayala2023 <jayalapadilla@student.neumont.edu>
2024-03-07 05:00:20 -0700

    Continue change

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,57 @@
-console.log('Website is running');
+const quotesArray = [];
 
-const quotes = document.getElementById('quotes');
-
-let apiUrl = `http://quotes.rest/qod.json`;
-
-fetch(apiUrl) // order the pizza
-.then(resp => resp.json())
-.then(data =>{
-    console.log(data);
-
-    // We can't do anything with the data from the API, until HERE
-    loadUsers(data.results);
+fetch("https://type.fit/api/quotes")
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data[1]);
+    //quotesArray = data
 });
 
-function loadUsers(users){
-    // Loop over the users
-    // For each uesr
+console.log(quotesArray);
+// let apiUrl = `https://zenquotes.io/api/today`;
+//https://zenquotes.io/api/random
+
+// fetch(apiUrl) // order the pizza
+// .then(resp => resp.json())
+// .then(data =>{
+//     console.log(data);
+
+//     // We can't do anything with the data from the API, until HERE
+//     loadUsers(data.results);
+//     console.log(data.results);
+// });
+
+// function loadUsers(users){
+//     // Loop over the users
+//     // For each uesr
     
-    console.log("Printing Users");
+//     console.log("Printing Users");
 
-    let html = "";
+//     let html = "";
 
-    for(let key in users){
-        console.log(users[key]);
+//     for(let key in users){
+//         console.log(users[key]);
 
-        let user = users[key];
+//         let user = users[key];
 
-        // update the DOM with our userBox HTML?
-        html += `
-            <div class="userBox">
-                    <div>
-                        <div>
-                            <label>Quote:</label> 
-                            ${user.quote}
-                        </div>
-                        <div>
-                            <label>Author:</label> ${user.author}
-                        </div>
+//         // update the DOM with our userBox HTML?
+//         html += `
+//             <div class="userBox">
+//                     <div>
+//                         <div>
+//                             <label>Quote:</label> 
+//                             ${user.quote}
+//                         </div>
+//                         <div>
+//                             <label>Author:</label> ${user.author}
+//                         </div>
                     
-                    </div>
-                </div>
-        `;
-    }
+//                     </div>
+//                 </div>
+//         `;
+//     }
 
-    userContainer.innerHTML = html;
-}
+//     userContainer.innerHTML = html;
+// }
